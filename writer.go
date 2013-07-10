ab24cfaee99641b3436e146eddd01652b3e5d29b
Lars Grote <lagrote@gmail.com>
2013-07-10 22:47:10 +0200

    make border and crop lines configurable

--- writer.go
+++ writer.go
@@ -7,35 +7,41 @@ import (
 )
 
 const (
-	Space         pdf.Unit = 0.00 * pdf.Cm
-	CropSpace     pdf.Unit = 0.5 * pdf.Cm
-	MarkLengh     pdf.Unit = 0.5 * pdf.Cm
-	CardW         pdf.Unit = 2.5 * pdf.Inch
-	CardH         pdf.Unit = 3.5 * pdf.Inch
-	PicBorder     pdf.Unit = 0.165 * pdf.Cm
-	Mm            pdf.Unit = 0.1 * pdf.Cm
-	BorderWidth   pdf.Unit = 0.4 * pdf.Cm
-	pdfColumns    int      = 3
-	pdfRows       int      = 3
-	pdfImgPerPage int      = pdfColumns * pdfRows
-	MarginBottom  pdf.Unit = (pdf.A4Height - (pdf.Unit(pdfRows)*CardH + (pdf.Unit(pdfRows)-1)*Space)) / 2
-	MarginLeft    pdf.Unit = (pdf.A4Width - (pdf.Unit(pdfColumns)*CardW + (pdf.Unit(pdfColumns)-1)*Space)) / 2
+	pageWidth         pdf.Unit = pdf.A4Width
+	pageHeight        pdf.Unit = pdf.A4Height
+	space             pdf.Unit = 0.00 * pdf.Cm
+	cropSpace         pdf.Unit = 0.5 * pdf.Cm
+	markLength        pdf.Unit = 0.5 * pdf.Cm
+	cardWidth         pdf.Unit = 2.5 * pdf.Inch
+	cardHeight        pdf.Unit = 3.5 * pdf.Inch
+	cardBorderPadding pdf.Unit = 0.165 * pdf.Cm
+	mm                pdf.Unit = 0.1 * pdf.Cm
+	borderWidth       pdf.Unit = 0.4 * pdf.Cm
+	columns           int      = 3
+	rows              int      = 3
+	cardsPerPage      int      = columns * rows
+	marginBottom      pdf.Unit = (pdf.A4Height - (pdf.Unit(rows)*cardHeight + (pdf.Unit(rows)-1)*space)) / 2
+	marginLeft        pdf.Unit = (pdf.A4Width - (pdf.Unit(columns)*cardWidth + (pdf.Unit(columns)-1)*space)) / 2
 )
 
 // retuns a new pdf writer. This writer writes Images to pdf file the dimensions are definte as constants
 func NewPdfWriter(writer io.Writer) PdfWriter {
 	return PdfWriter{
-		doc:    pdf.New(),
-		writer: writer,
+		doc:       pdf.New(),
+		writer:    writer,
+		Border:    true,
+		CropLines: true,
 	}
 }
 
 type PdfWriter struct {
-	writer   io.Writer
-	doc      *pdf.Document
-	page     *pdf.Canvas
-	imgCount int
-	current  pdf.Point
+	Border    bool
+	CropLines bool
+	writer    io.Writer
+	doc       *pdf.Document
+	page      *pdf.Canvas
+	imgCount  int
+	current   pdf.Point
 }
 
 // writes an Image  n-times to the pdf file. With crop lines and black border
@@ -43,9 +49,13 @@ func (this *PdfWriter) WriteImage(img image.Image, count int) {
 	ref := this.addImage(img)
 	for i := 0; i < count; i++ {
 		this.setCurrentPoint()
-		this.drawCropLines()
+		if this.CropLines {
+			this.drawCropLines()
+		}
 		this.drawImageReference(ref)
-		this.drawBorder()
+		if this.Border {
+			this.drawBorder()
+		}
 		this.imgCount++
 	}
 }
@@ -53,16 +63,16 @@ func (this *PdfWriter) WriteImage(img image.Image, count int) {
 // based on the imageCount the lower left corner of the next image is computed
 func (this *PdfWriter) setCurrentPoint() pdf.Point {
 	switch {
-	case this.imgCount%pdfImgPerPage == 0:
+	case this.imgCount%cardsPerPage == 0:
 		this.newPage()
-		this.current = pdf.Point{X: MarginLeft, Y: MarginBottom + ((pdf.Unit(pdfRows - 1)) * Space) + (pdf.Unit((pdfRows - 1)) * CardH)}
+		this.current = pdf.Point{X: marginLeft, Y: marginBottom + ((pdf.Unit(rows - 1)) * space) + (pdf.Unit((rows - 1)) * cardHeight)}
 		return this.current
 
-	case this.imgCount%pdfColumns == 0:
-		this.current = pdf.Point{X: MarginLeft, Y: this.current.Y - Space - CardH}
+	case this.imgCount%columns == 0:
+		this.current = pdf.Point{X: marginLeft, Y: this.current.Y - space - cardHeight}
 		return this.current
 	}
-	this.current = pdf.Point{X: this.current.X + Space + CardW, Y: this.current.Y}
+	this.current = pdf.Point{X: this.current.X + space + cardWidth, Y: this.current.Y}
 	return this.current
 }
 
@@ -74,20 +84,20 @@ func (this *PdfWriter) addImage(img image.Image) pdf.Reference {
 // draws an ImageReference in the pdf. The Image to be drawn needs to be added to the file first.
 func (this *PdfWriter) drawImageReference(ref pdf.Reference) {
 	this.page.DrawImageReference(ref, pdf.Rectangle{
-		Min: pdf.Point{X: this.current.X + PicBorder, Y: this.current.Y + PicBorder},
-		Max: pdf.Point{X: this.current.X + CardW - PicBorder, Y: this.current.Y + CardH - PicBorder},
+		Min: pdf.Point{X: this.current.X + cardBorderPadding, Y: this.current.Y + cardBorderPadding},
+		Max: pdf.Point{X: this.current.X + cardWidth - cardBorderPadding, Y: this.current.Y + cardHeight - cardBorderPadding},
 	})
 }
 
 // draws a black border based on the current point
 func (this *PdfWriter) drawBorder() {
-	this.page.SetLineWidth(BorderWidth)
+	this.page.SetLineWidth(borderWidth)
 	path := new(pdf.Path)
-	path.Move(pdf.Point{this.current.X + Mm, this.current.Y + Mm})
-	path.Line(pdf.Point{this.current.X + Mm, this.current.Y + CardH - Mm})
-	path.Line(pdf.Point{this.current.X + CardW - Mm, this.current.Y + CardH - Mm})
-	path.Line(pdf.Point{this.current.X + CardW - Mm, this.current.Y + Mm})
-	path.Line(pdf.Point{this.current.X + Mm - BorderWidth/2, this.current.Y + Mm})
+	path.Move(pdf.Point{this.current.X + mm, this.current.Y + mm})
+	path.Line(pdf.Point{this.current.X + mm, this.current.Y + cardHeight - mm})
+	path.Line(pdf.Point{this.current.X + cardWidth - mm, this.current.Y + cardHeight - mm})
+	path.Line(pdf.Point{this.current.X + cardWidth - mm, this.current.Y + mm})
+	path.Line(pdf.Point{this.current.X + mm - borderWidth/2, this.current.Y + mm})
 	this.page.Stroke(path)
 }
 
@@ -105,20 +115,20 @@ func (this *PdfWriter) drawCropLines() {
 	for i := 0; i < 2; i++ {
 		val := pdf.Pt * pdf.Unit(float32(i))
 		// left
-		drawCropLine(pdf.Point{this.current.X, this.current.Y + CardH*val},
-			pdf.Point{this.current.X - CropSpace, this.current.Y + CardH*val})
+		drawCropLine(pdf.Point{this.current.X, this.current.Y + cardHeight*val},
+			pdf.Point{this.current.X - cropSpace, this.current.Y + cardHeight*val})
 
 		// top
-		drawCropLine(pdf.Point{this.current.X + CardW*val, this.current.Y + CardH},
-			pdf.Point{this.current.X + CardW*val, this.current.Y + CardH + CropSpace})
+		drawCropLine(pdf.Point{this.current.X + cardWidth*val, this.current.Y + cardHeight},
+			pdf.Point{this.current.X + cardWidth*val, this.current.Y + cardHeight + cropSpace})
 
 		// right
-		drawCropLine(pdf.Point{this.current.X + CardW, this.current.Y + CardH*val},
-			pdf.Point{this.current.X + CardW + CropSpace, this.current.Y + CardH*val})
+		drawCropLine(pdf.Point{this.current.X + cardWidth, this.current.Y + cardHeight*val},
+			pdf.Point{this.current.X + cardWidth + cropSpace, this.current.Y + cardHeight*val})
 
 		// top
-		drawCropLine(pdf.Point{this.current.X + CardW*val, this.current.Y},
-			pdf.Point{this.current.X + CardW*val, this.current.Y - CropSpace})
+		drawCropLine(pdf.Point{this.current.X + cardWidth*val, this.current.Y},
+			pdf.Point{this.current.X + cardWidth*val, this.current.Y - cropSpace})
 	}
 }
 
@@ -127,15 +137,14 @@ func (this *PdfWriter) newPage() {
 	if this.page != nil {
 		this.page.Close()
 	}
-	this.page = this.doc.NewPage(pdf.A4Width, pdf.A4Height)
+	this.page = this.doc.NewPage(pageWidth, pageHeight)
 }
 
 // the last page is closed. The objects are encoded to a pdf file and written to the given writer
 func (this *PdfWriter) Close() error {
-
-	if this.page != nil {
-		this.page.Close()
+	if this.page == nil {
+		this.newPage()
 	}
-
+	this.page.Close()
 	return this.doc.Encode(this.writer)
 }
