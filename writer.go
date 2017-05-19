fa4a4c2002110b5c666b5b17fad48304b0416f46
Lars Grote <lgr@softwerke.com>
2017-05-19 14:06:24 +0200

    use Writer on Output
    * return PdfWriter as Reference
    * use only name for image referencing
    * docs

--- writer.go
+++ writer.go
@@ -7,12 +7,19 @@ import (
 )
 
 const (
-	Pt                Unit = 1
-	Inch              Unit = 72
-	Cm                Unit = 28.35
-	Mm                Unit = Cm / 10
-	A4Width           Unit = 210 * Mm
-	A4Height          Unit = 297 * Mm
+	// Pt is one Point
+	Pt Unit = 1
+	// Inch is 72 Points
+	Inch Unit = 72
+	// Cm is 28.85 Points
+	Cm Unit = 28.35
+	// Mm is Cm/10
+	Mm Unit = Cm / 10
+	// A4Width in Points
+	A4Width Unit = 210 * Mm
+	// A4Height in Points
+	A4Height Unit = 297 * Mm
+
 	pageWidth         Unit = A4Width
 	pageHeight        Unit = A4Height
 	space             Unit = 0.00 * Mm
@@ -29,8 +36,7 @@ const (
 	defaultMargin     Unit = 20 * Mm
 )
 
-// Unit is a device-independent dimensional type.  On a new canvas, this
-// represents 1/72 of an inch.
+// Unit is a device-independent dimensional type. This represents 1/72 of an inch.
 type Unit float32
 
 // Point is a 2D point.
@@ -38,18 +44,17 @@ type Point struct {
 	X, Y Unit
 }
 
-// retuns a new pdf writer. This writer writes Images to pdf file
+// NewPdfWriter creates a new pdf writer. The writer writes Images to pdf file
 // the dimensions are definte as constants
-func NewPdfWriter(writer io.Writer) PdfWriter {
+func NewPdfWriter() *PdfWriter {
 	pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.SetTitle("Your Proxies", true)
 	pdf.SetAuthor("proxy-mat.appspot.com", true)
 	pdf.SetDrawColor(0, 0, 0)
 	pdf.SetCompression(false)
 
-	return PdfWriter{
+	return &PdfWriter{
 		doc:         pdf,
-		writer:      writer,
 		Border:      true,
 		CropLines:   true,
 		Columns:     columns,
@@ -61,44 +66,47 @@ func NewPdfWriter(writer io.Writer) PdfWriter {
 	}
 }
 
+// PdfWriter can write images to a pdf with borders and crop lines
 type PdfWriter struct {
-	Border, CropLines     bool
-	Columns, Rows         int
+	// Should the Image have a black Border? Should crop lines be drawn?
+	Border, CropLines bool
+	// The number of columns/rows per page
+	Columns, Rows int
+	// the size of a page
 	PageWidth, PageHeight Unit
-	BorderWidth, Space    Unit
+	// how thick is the border. The space between images (default to 0.0)
+	BorderWidth, Space Unit
 
-	writer   io.Writer
 	doc      *gofpdf.Fpdf
 	imgCount int
 	current  Point
 }
 
-// writes an Image  n-times to the pdf file. With crop lines and black border
+// WriteImage writes an Image  n-times to the pdf file. With crop lines and black border.
+// The name is used as identifier, so each image is only  added to the file once and afterwards refernced.
 func (w *PdfWriter) WriteImage(r io.Reader, name string, count int) {
 
-	ref := w.addImage(r, name)
+	w.addImage(r, name)
 	for i := 0; i < count; i++ {
-		w.setCurrentPoint()
+		w.incrPosition()
 		if w.CropLines {
 			w.drawCropLines()
 		}
-		w.drawImageReference(name, ref)
+		w.drawImage(name)
 		if w.Border {
 			w.drawBorder()
 		}
 		w.imgCount++
 	}
-
 }
 
-// the last page is closed. The objects are encoded to
-// a pdf file and written to the given writer
-func (w *PdfWriter) Close() error {
-	return w.doc.Output(w.writer)
+// Output encodes the everything that has been added so far to a pdf and writes it to the given Writer.
+func (w *PdfWriter) Output(writer io.Writer) error {
+	return w.doc.Output(writer)
 }
 
-// based on the imageCount the lower left corner of the next image is computed
-func (w *PdfWriter) setCurrentPoint() {
+// incrPosition calculates the lower left corner of the next image based on the imageCount
+func (w *PdfWriter) incrPosition() {
 	switch {
 	case w.imgCount%w.cardsPerPage() == 0:
 		w.newPage()
@@ -118,8 +126,8 @@ func (w *PdfWriter) setCurrentPoint() {
 		Y: w.current.Y}
 }
 
-// adds an Image to the pdf and returns its reference. Note that the Image
-// isn't yet drawn, it's just added.
+//addImage adds an Image to the pdf and returns its reference. Note that the Image
+// isn't yet drawn, it's just added to the file.
 func (w *PdfWriter) addImage(r io.Reader, name string) *gofpdf.ImageInfoType {
 	iit := w.doc.RegisterImageOptionsReader(name, gofpdf.ImageOptions{
 		ImageType: "JPG",
@@ -129,10 +137,9 @@ func (w *PdfWriter) addImage(r io.Reader, name string) *gofpdf.ImageInfoType {
 	return iit
 }
 
-// draws an ImageReference in the pdf. The Image to be drawn needs to be added
-// to the file first.
-
-func (w *PdfWriter) drawImageReference(name string, ref *gofpdf.ImageInfoType) {
+// drawImage draws a visible image in the pdf. The Image to be drawn needs to be added
+// to the file first. It's referenced by name.
+func (w *PdfWriter) drawImage(name string) {
 	w.doc.ImageOptions(name,
 		float64((w.current.X+w.cardBorderPadding())/Mm),
 		float64((w.current.Y+w.cardBorderPadding())/Mm),
@@ -145,7 +152,7 @@ func (w *PdfWriter) drawImageReference(name string, ref *gofpdf.ImageInfoType) {
 		}, 0, "")
 }
 
-// draws a black border based on the current point
+// draws a black border based on the current position
 func (w *PdfWriter) drawBorder() {
 	w.doc.SetLineWidth(float64(w.BorderWidth / Mm))
 
@@ -159,17 +166,18 @@ func (w *PdfWriter) drawBorder() {
 	w.doc.DrawPath("D")
 }
 
+// draws a line from the current position to the given coordinates.
 func (w *PdfWriter) lineTo(x, y Unit) {
 	w.doc.LineTo(float64(x/Mm), float64(y/Mm))
 }
 
-// draw a single line
+// draw a single line from point to point
 func (w *PdfWriter) drawLine(from, to Point) {
 	w.doc.MoveTo(float64(from.X/Mm), float64(from.Y/Mm))
 	w.lineTo(to.X, to.Y)
 }
 
-// draws the crop lines based on the current point (8 per card)
+// draws crop lines based on the current position (8 per card)
 func (w *PdfWriter) drawCropLines() {
 	w.doc.SetLineWidth(float64(cropLineWidth))
 	for i := 0; i < 2; i++ {
@@ -236,15 +244,18 @@ func (w *PdfWriter) newPage() {
 	w.doc.AddPage()
 }
 
+// number of cards that can be added to a single page (Rows x Columns)
 func (w *PdfWriter) cardsPerPage() int {
 	return w.Columns * w.Rows
 }
 
+// the margin from the bottom of the page (same as from top of the page)
 func (w *PdfWriter) marginBottom() Unit {
 	return (w.PageHeight - (Unit(w.Rows)*cardHeight +
 		(Unit(w.Rows)-1)*space)) / 2
 }
 
+// the margin from the left of the page (same as from right of the page)
 func (w *PdfWriter) marginLeft() Unit {
 	return (w.PageWidth - (Unit(w.Columns)*cardWidth +
 		(Unit(w.Columns)-1)*space)) / 2
